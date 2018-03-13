8b2d0248ae4bb652b8de3264b5e79168a23500b6
Jeffrey Zhen <jzhen91@gmail.com>
2018-03-12 20:00:04 -0400

    posts index renders posts

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,11 +1,46 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
+// import { bindActionCreators } from 'redux'; // We don't need this as connect will just do it for us
 
 class PostsIndex extends Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      )
+    })
+  }
+
   render() {
     return (
-      <div>Posts Index</div>
+      <div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
+      </div>
     );
   }
 }
 
-export default PostsIndex;
+// Whenever we want to consume anything from application level state, we need to define this function
+function mapStateToProps({ posts }) {
+  return { posts }
+}
+
+//**** THESE ALL DO THE SAME THING ****//
+// function mapDispatchToProps(dispatch) {
+//   return bindActionCreators({ fetchPosts}, dispatch);
+// }
+// export default connect(null, mapDispatchToProps)(PostsIndex);
+
+// export default connect(null, { fetchPosts: fetchPosts })(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
